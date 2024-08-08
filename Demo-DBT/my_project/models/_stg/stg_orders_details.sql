cc487bd40f5892fac619985aa4b33529ebcd8391
Enjie <anggisetiawandc@gmail.com>
2024-08-09 00:23:56 +0700

    DBT Commit

--- Demo-DBT/my_project/models/_stg/stg_orders_details.sql
+++ Demo-DBT/my_project/models/_stg/stg_orders_details.sql
@@ -1,7 +1,7 @@
 select
-order_details_id:: int as order_details_id
-, product_id::int as order_id
-, product_id::int as product_id
-, quantity as order_qty
-, price as unit_sales
-from{{ source ('store', 'orders_details')}}
+  order_details_id::int as order_details_id,
+  order_id::int as order_id,
+  product_id::int as product_id,
+  quantity as order_qty,
+  price as unit_sales
+from {{ source('store', 'orders_details') }}
